0be21053dbe2d51a9bd8245aa86e0e61fcdd9397
joshkmcdonald <joshkmcdonald@gmail.com>
2017-08-07 21:30:34 -0600

    Add associations between rooms and users

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,2 +1,4 @@
 class Room < ApplicationRecord
+  has_one :creator, :class_name => "User", :foreign_key => "user_id"
+  has_many :users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :created_rooms, :class_name => "Room", :foreign_key => "room_id"
 end

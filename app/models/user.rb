2562456c6c386700e71b62d40b5ec3eee37bd388
joshkmcdonald <joshkmcdonald@gmail.com>
2017-08-10 00:03:00 -0600

    Add first and last name methods to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,5 +13,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :rooms
+  has_many :rooms, dependent: :destroy
+
+  validates_presence_of :name
+
+  def first_name
+    self.name.split.first
+  end
+
+  def last_name
+    self.name.split.last
+  end
 end
